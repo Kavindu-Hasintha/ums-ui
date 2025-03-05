c5d811ef03ae14a28da650a9d25bd2904505f7f3
Kavindu-Hasintha <110930930+Kavindu-Hasintha@users.noreply.github.com>
2025-03-05 10:43:05 +0530

    auth utils added with setSession and getSession func

---
+++ src/auth/auth.utils.ts
@@ -0,0 +1,15 @@
+import axiosInstance from "../utils/axiosInstance.ts";
+
+export const setSession = (accessToken: string | null) => {
+    if (accessToken) {
+        localStorage.setItem('accessToken', accessToken);
+        axiosInstance.defaults.headers.common.Authorization = `Bearer ${accessToken}`;
+    } else {
+        localStorage.removeItem('accessToken');
+        delete axiosInstance.defaults.headers.common.Authorization;
+    }
+};
+
+export const getSession = () => {
+    return localStorage.getItem('accessToken');
+}
