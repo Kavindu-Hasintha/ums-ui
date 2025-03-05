57fdcf7be825f6eb65a0ae4a006bb6cac5c9450d
Kavindu-Hasintha <110930930+Kavindu-Hasintha@users.noreply.github.com>
2025-03-05 10:50:24 +0530

    allowedRolesForUpdateArray func implemented

--- src/auth/auth.utils.ts
+++ src/auth/auth.utils.ts
@@ -1,4 +1,5 @@
 import axiosInstance from "../utils/axiosInstance.ts";
+import {IAuthUser, RolesEnum} from "../types/auth.types.ts";
 
 export const setSession = (accessToken: string | null) => {
     if (accessToken) {
@@ -13,3 +14,15 @@ export const setSession = (accessToken: string | null) => {
 export const getSession = () => {
     return localStorage.getItem('accessToken');
 }
+
+export const allAccessRoles = [RolesEnum.OWNER, RolesEnum.ADMIN, RolesEnum.MANAGER, RolesEnum.USER];
+export const managerAccessRoles = [RolesEnum.OWNER, RolesEnum.ADMIN, RolesEnum.MANAGER];
+export const adminAccessRoles = [RolesEnum.OWNER, RolesEnum.ADMIN];
+export const ownerAccessRoles = [RolesEnum.OWNER];
+
+// We need to specify which roles can be updated by Logged-in user
+export const allowedRolesForUpdateArray = (loggedInUser?: IAuthUser): string[] => {
+    return loggedInUser?.roles.includes(RolesEnum.OWNER)
+        ? [RolesEnum.ADMIN, RolesEnum.MANAGER, RolesEnum.USER]
+        : [RolesEnum.MANAGER, RolesEnum.USER];
+};
