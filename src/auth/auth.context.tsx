5b174030d1adaf4bda7cb5c137df72a28dce87dd
Kavindu-Hasintha <110930930+Kavindu-Hasintha@users.noreply.github.com>
2025-03-06 11:46:15 +0530

    register func implemented

--- src/auth/auth.context.tsx
+++ src/auth/auth.context.tsx
@@ -1,5 +1,16 @@
-import {IAuthContext, IAuthContextAction, IAuthContextActionTypes, IAuthContextState} from "../types/auth.types.ts";
-import {createContext, ReactNode} from "react";
+import {
+    IAuthContext,
+    IAuthContextAction,
+    IAuthContextActionTypes,
+    IAuthContextState,
+    ILoginResponseDto
+} from "../types/auth.types.ts";
+import {createContext, ReactNode, useCallback, useEffect, useReducer} from "react";
+import {useNavigate} from "react-router-dom";
+import {getSession, setSession} from "./auth.utils.ts";
+import axiosInstance from "../utils/axiosInstance.ts";
+import {ME_URL, PATH_AFTER_REGISTER, REGISTER_URL} from "../utils/globalConfig.ts";
+import toast from "react-hot-toast";
 
 // We need a reducer function for useReducer hook
 const authReducer = (state: IAuthContextState, action: IAuthContextAction) => {
@@ -36,3 +47,63 @@ export const AuthContext = createContext<IAuthContext | null>(null);
 interface IProps {
     children: ReactNode;
 }
+
+// We create a component to manage all auth functionalities and export it and use it
+const AuthContextProvider = ({ children }: IProps) => {
+    const [state, dispatch] = useReducer(authReducer, initialAuthState);
+    const navigate = useNavigate();
+
+    // Initialize Method
+    const initializeAuthContext = useCallback(async () => {
+        try {
+            const token = getSession();
+            if (token) {
+                // validate accessToken by calling backend
+                const response = await axiosInstance.post<ILoginResponseDto>(ME_URL, {
+                    token,
+                });
+                // In response, we receive jwt token and user data
+                const { newToken, userInfo } = response.data;
+                setSession(newToken);
+                dispatch({
+                    type: IAuthContextActionTypes.LOGIN,
+                    payload: userInfo,
+                });
+            } else {
+                setSession(null);
+                dispatch({
+                    type: IAuthContextActionTypes.LOGOUT,
+                });
+            }
+        } catch (error) {
+            setSession(null);
+            dispatch({
+                type: IAuthContextActionTypes.LOGOUT,
+            });
+        }
+    }, []);
+
+    // In start of application, we call initializeAuthContext to be sure about authentication status
+    useEffect(() => {
+        console.log('AuthContext Initialization start');
+        initializeAuthContext()
+            .then(() => console.log('initializeAuthContext was successful'))
+            .catch((error) => console.log(error));
+    }, []);
+
+    // Register Method
+    const register = useCallback(
+        async (firstName: string, lastName: string, userName: string, email: string, password: string, address: string) => {
+            const response = await  axiosInstance.post(REGISTER_URL, {
+                firstName,
+                lastName,
+                userName,
+                email,
+                password,
+                address,
+            });
+            console.log('Register Result: ', response);
+            toast.success('Register was successful. Please Login.');
+            navigate(PATH_AFTER_REGISTER);
+        }, []);
+};
