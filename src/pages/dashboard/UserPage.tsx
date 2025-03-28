6b70415e7bb1fe2ff42e98a6aa6204ec59e48fb3
Kavindu-Hasintha <110930930+Kavindu-Hasintha@users.noreply.github.com>
2025-03-28 17:46:00 +0530

    Owner and User pages implemented

--- src/pages/dashboard/UserPage.tsx
+++ src/pages/dashboard/UserPage.tsx
@@ -1,9 +1,12 @@
+import PageAccessTemplate from '../../components/dashboard/page-access/PageAccessTemplate';
+import { FaUser } from 'react-icons/fa';
+
 const UserPage = () => {
     return (
-        <div>
-            User page
+        <div className='pageTemplate2'>
+            <PageAccessTemplate color='#FEC223' icon={FaUser} role='User' />
         </div>
     );
-}
+};
 
 export default UserPage;
